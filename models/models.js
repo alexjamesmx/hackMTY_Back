23bb5faa6e6171661a72182f48c91b0b8da6da69
miguelmendozajr <miguelmendozaj20@gmail.com>
2024-09-14 23:20:31 -0600

    fix event schema

--- models/models.js
+++ models/models.js
@@ -22,22 +22,20 @@ const productSchema = new Schema({
   company: {type: Schema.Types.ObjectId, ref: "Company" }
 });
 
-const requirementSchema = new Schema({
-  product: {type: Schema.Types.ObjectId, ref: "Product" },
-  units: { type: Number, default: 1},
-  paid: { type: Boolean, default: false },
-  paidBy: { type: Schema.Types.ObjectId, ref: "User" }
-})
-
 const eventSchema = new Schema({
   name: { type: String, required: true },
   start_date: { type: Date, required: true },
   end_date: { type: Date, required: true },
-  requirements: [requirementSchema],
+  requirements: [{
+    product: {type: Schema.Types.ObjectId, ref: "Product" },
+    units: { type: Number, default: 1},
+    paid: { type: Boolean, default: false },
+    paidBy: { type: Schema.Types.ObjectId, ref: "User" }
+  }],
   equitative: {type: Boolean, required: true},
   admin: {type: Schema.Types.ObjectId, ref: "User", required: true},
   members: [{
-    user: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    user: {type: Schema.Types.ObjectId, ref: "User"},
     paid: { type: Number, default: 0 }
   }]
 });
@@ -45,6 +43,5 @@ const eventSchema = new Schema({
 // Crear modelos
 export const User = mongoose.model("User", userSchema);
 export const Company = mongoose.model("Company", companySchema);
-export const Requirement = mongoose.model("Requirement", requirementSchema);
 export const Product = mongoose.model("Product", productSchema);
 export const Event = mongoose.model("Event", eventSchema);
